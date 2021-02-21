05615d1bfe8a44230944b966c42fe13d2491c0d4
Jack Walker <74609541+notpengy@users.noreply.github.com>
2021-02-21 13:08:21 +0000

    Source Code

---
+++ Grocery Web App/src/java/pojo/Item.java
@@ -0,0 +1,27 @@
+package pojo;
+
+public class Item {
+
+    private String productName;
+    private double productPrice;
+
+    public Item() {
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getProdcutPrice() {
+        return productPrice;
+    }
+
+    public void setProdcutPrice(double prodcutPrice) {
+        this.productPrice = prodcutPrice;
+    }
+
+}
